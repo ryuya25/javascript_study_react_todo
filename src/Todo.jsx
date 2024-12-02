3d3467ecae44b875594b8c693d743541b38baae3
ryuya25 <kagiya20100708@gmail.com>
2024-12-02 15:43:26 +0900

    55. CSSでスタイリング

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,40 +3,47 @@ import './styles.css';
 export const Todo = () =>{
   return (
     <>
-      <div>
+      <div className ="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
-
       </div>
-      <div>
-        <p>未完了のTODO</p>
+
+      <div className='incomplete-area'>
+        <p className='title'>未完了のTODO</p>
           <ul>
           <li>
-              <p>TODOです</p>
+            <div className='list-row'>
+              <p className="todo-item">TODOです</p>
               <button>完了</button>
               <button>削除</button>
-            </li>
-            <li>
-              <p>TODOです</p>
-              <button>完了</button>
-              <button>削除</button>
-            </li>
-          </ul>
+            </div>
+          </li>
+          <li>
+            <div className='list-row'>
+              <p className='todo-item'>TODOです</p>
+                <button>完了</button>
+                <button>削除</button>
+                </div>
+          </li>
+        </ul>
+      </div>
 
-              </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className='complete-area'>
+        <p className='title'>完了のTODO</p>
           <ul>
             <li>
-              <p>TODOでした</p>
+            <div className='list-row'>
+              <p className='todo-item'>TODOでした</p>
               <button>戻す</button>
+              </div>
             </li>
             <li>
-              <p>TODOでした</p>
+            <div className='list-row'>
+              <p className='todo-item'>TODOでした</p>
               <button>戻す</button>
+              </div>
             </li>
           </ul>
-
       </div>
     </>
   );
