2b79cbb0095a30eea35f43df03012230b715930b
ryuya25 <kagiya20100708@gmail.com>
2024-12-02 15:17:35 +0900

    54. JSXで構造を作成

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,9 +1,43 @@
-import './App.css';
+import './styles.css';
 
-export const  Todo=() =>{
+export const Todo = () =>{
   return (
     <>
-      <div>Initialize Code</div>
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+          <ul>
+          <li>
+              <p>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </li>
+            <li>
+              <p>TODOです</p>
+              <button>完了</button>
+              <button>削除</button>
+            </li>
+          </ul>
+
+              </div>
+      <div>
+        <p>完了のTODO</p>
+          <ul>
+            <li>
+              <p>TODOでした</p>
+              <button>戻す</button>
+            </li>
+            <li>
+              <p>TODOでした</p>
+              <button>戻す</button>
+            </li>
+          </ul>
+
+      </div>
     </>
   );
 }
